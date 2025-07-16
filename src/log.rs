03e083651c92d6813002872fd1280be02cc03013
pml68 <contact@pml68.dev>
2025-07-17 00:21:15 +0200

    feat: add support for setting STDOUT log level with `RUST_LOG` variable

--- src/log.rs
+++ src/log.rs
@@ -1,5 +1,5 @@
 mod logger {
-    use std::fs::File;
+    use std::{env, fs::File};
     use tracing::Level;
     pub use tracing::{debug, error, info, trace, warn};
     use tracing_subscriber::{
@@ -8,9 +8,19 @@ mod logger {
         prelude::*,
     };
 
-    pub fn init() -> std::io::Result<()> {
+    pub fn init() -> Result<(), Box<dyn std::error::Error>> {
+        let env_rust_log = env::var("RUST_LOG")
+            .ok()
+            .as_deref()
+            .map(str::parse::<Level>)
+            .transpose()?;
+
         tracing_subscriber::registry()
-            .with(fmt::layer().compact())
+            .with(
+                fmt::layer()
+                    .compact()
+                    .with_filter(LevelFilter::from_level(env_rust_log.unwrap_or(Level::INFO))),
+            )
             .with(
                 fmt::layer()
                     .with_writer(File::create("debug_log.json")?)
